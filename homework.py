294c3eba0d375c691849585455604921b2f6963c
Andrey_Ivantsiv <andrey.ivantsiv@gmail.com>
2021-02-15 13:49:54 +1100

    Исправление замечаний ревьюера 15.02.21

--- homework.py
+++ homework.py
@@ -1,7 +1,9 @@
 import datetime as dt
 from datetime import timedelta as td
 
-now_date = dt.datetime.now().date()
+
+def today():
+    return dt.datetime.now().date()
 
 
 class Calculator:
@@ -10,73 +12,70 @@ class Calculator:
         self.records = []
 
     def add_record(self, rec):
+        """Создает список записей"""
         self.records.append(rec)
         return self.records
 
     def get_today_stats(self):
+        """Статистика за день"""
         self.today_stats = 0
-        for record in self.records:
-            if record.date == now_date:
-                self.today_stats += record.amount
+        now_date = today()
+        self.today_stats = sum(
+            [record.amount for record in self.records
+                if record.date == now_date])
         return self.today_stats
 
     def get_week_stats(self):
+        """Статистика за последние 7 дней"""
         self.week_stats = 0
-        for record in self.records:
-            if now_date >= record.date >= (now_date - td(days=7)):
-                self.week_stats += record.amount
+        now_date = today()
+        self.week_stats = sum(
+            [record.amount for record in self.records
+                if now_date >= record.date >= (now_date - td(days=6))])
         return self.week_stats
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        calories_remained = 0
+        """Получаем остаток по каллориям на день"""
         if self.limit > self.get_today_stats():
             calories_remained = self.limit - self.get_today_stats()
             return ('Сегодня можно съесть что-нибудь ещё, '
                     'но с общей калорийностью не более '
                     f'{calories_remained} кКал')
-        else:
-            return ('Хватит есть!')
+        return ('Хватит есть!')
 
 
 class CashCalculator(Calculator):
     USD_RATE = 73.65
     EURO_RATE = 89.25
+    RUB_RATE = 1
 
     def get_today_cash_remained(self, currency):
+        """Получаем информацию об остатках денег на день"""
+
+        dict = {'usd': {self.USD_RATE: 'USD'},
+                'eur': {self.EURO_RATE: 'Euro'},
+                'rub': {self.RUB_RATE: 'руб'}
+                }
         cash_value_deafult = round((self.limit) - (self.get_today_stats()), 2)
-        cash_value_usd = round((cash_value_deafult / self.USD_RATE), 2)
-        cash_value_euro = round((cash_value_deafult / self.EURO_RATE), 2)
-        if cash_value_deafult > 0:
-            if currency == 'usd':
-                return (f'На сегодня осталось {cash_value_usd} USD')
-            elif currency == 'eur':
-                return (f'На сегодня осталось {cash_value_euro} Euro')
-            elif currency == 'rub':
-                return (f'На сегодня осталось {cash_value_deafult} руб')
+        for key, value in dict[currency].items():
+            cash_value = round(cash_value_deafult / key, 2)
+            if cash_value > 0:
+                return (f'На сегодня осталось {cash_value} {value}')
+            elif cash_value < 0:
+                return (f'Денег нет, держись: твой долг - '
+                        f'{abs(cash_value)} {value}')
             else:
-                return ('Введи валюту верно: usd, eur, rub')
-        elif cash_value_deafult < 0:
-            if currency == 'usd':
-                return (f'Денег нет, держись: твой долг - '
-                        f'{abs(cash_value_usd)} USD')
-            elif currency == 'eur':
-                return (f'Денег нет, держись: твой долг - '
-                        f'{abs(cash_value_euro)} Euro')
-            elif currency == 'rub':
-                return (f'Денег нет, держись: твой долг - '
-                        f'{abs(cash_value_deafult)} руб')
-            else:
-                return ('Введи валюту верно: usd, eur, rub')
-        else:
-            return ('Денег нет, держись')
+                return ('Денег нет, держись')
 
 
 class Record:
-    def __init__(self, amount, comment, date=now_date):
+    def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
         self.date = date
         if type(self.date) == str:
             self.date = dt.datetime.strptime(self.date, '%d.%m.%Y').date()
+        else:
+            self.date = today()
